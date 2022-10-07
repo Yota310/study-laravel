fe218a873717c32d23889141e9c9bba00a71968b
Yota310 <jingangsihaiuei@gmail.com>
2022-10-07 21:12:05 +0900

    投稿内容の削除機能の追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,6 +17,14 @@
                        </h2>
                    <p class="body">{{$post->body}}</p>
                    
+            
+
+<form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+    @csrf
+    @method('DELETE')
+    <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+</form>
+                   
                </div>
                @endforeach
                <a href='/posts/create'>create</a>
@@ -24,5 +32,15 @@
             <div class="paginate">
                 {{ $posts->links()}}
             </div>
+            <script>
+           
+             function deletePost(id) {
+        'use strict'
+
+        if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+            document.getElementById(`form_${id}`).submit();
+        }
+    }
+            </script>
         </body>
         </html>
